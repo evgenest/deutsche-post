de37a37e91ae9ae0af0c9ab77fb07c3d73a73313
evgenest <evgenest@onmail.com>
2023-08-07 13:55:08 +0200

    trying to commit with error and/or bad formatting

--- src/pages/add-letter/index.js
+++ src/pages/add-letter/index.js
@@ -6,12 +6,15 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function AddLetter() {
+  // var 1/abc = 'abc'
   return (
     <AnyPage>
       <Button variant="contained" href="/">
         Main Page
       </Button>
       <FormGroupAddLetter />
-    </AnyPage>
+    </
+    AnyPage
+    >
   )
 }
