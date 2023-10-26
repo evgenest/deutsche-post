ba2e2e4449e7e6c0572e981decec9ffcfab1f96c
evgenest <evgenest@onmail.com>
2023-10-26 10:41:20 +0200

    AddButton added

---
+++ src/components/interface/buttons/AddButton.jsx
@@ -0,0 +1,27 @@
+export default function AddButton({ ...props }) {
+  return (
+    <button
+      {...props}
+      type="button"
+      // styles
+      class="mr-2 inline-flex items-center rounded-full bg-blue-700 p-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      <svg
+        class="h-6 w-6 text-white dark:text-gray-800"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 18 18"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 1v16M1 9h16"
+        />
+      </svg>
+      <span class="sr-only">Add a more one</span>
+    </button>
+  )
+}
